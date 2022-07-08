992f4db05d3bd98d2b4965d8f5f89047d6f6a875
Soobin Rho <soobinrho@gmail.com>
2022-07-07 21:29:30 -0500

    Add Hw1b.

---
+++ RhoArithmetic.java
@@ -0,0 +1,12 @@
+// Soobin Rho
+// Problem 2.15 | Public RhoArithmetic
+// July 7, 2022
+
+public class RhoArithmetic {
+    // 
+    public static void main(String[] args) {
+
+        // 
+
+    }
+}
---
+++ RhoCompare.java
@@ -0,0 +1,12 @@
+// Soobin Rho
+// Problem 2.15 | Public RhoCompare
+// July 7, 2022
+
+public class RhoCompare {
+    // 
+    public static void main(String[] args) {
+
+        // 
+
+    }
+}
