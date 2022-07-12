e9b4696efa99a95ccd0845e7f080a252ad95b417
Soobin Rho <soobinrho@gmail.com>
2022-07-11 23:06:09 -0500

    Move HW3 to competed folder.

---
+++ FinalProject.java
@@ -0,0 +1,29 @@
+// Soobin Rho
+// Final Project | Public RhoDB
+// July ?
+import java.util.Scanner;
+
+public class RhoDB {
+    /* Probably use java.sql
+     * I gotta learn about databases
+     * 1.
+     * 2.
+     * 3
+    */
+    public static void main(String[] args) {
+
+        // 1.
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter the first integer: ");
+        int input_1 = userInput.nextInt();
+        System.out.print("Enter the second integer: ");
+        int input_2 = userInput.nextInt();
+
+        // 2.
+
+
+        // 3.
+
+    } // End of method main
+} // End of class RhoDB
+
---
+++ Template.java
@@ -0,0 +1,28 @@
+// Soobin Rho
+// Problem 5.16 | Public RhoDigits
+// July 11, 2022
+import java.util.Scanner;
+
+public class RhoDigits {
+    /*
+     * 1.
+     * 2.
+     * 3
+    */
+    public static void main(String[] args) {
+
+        // 1.
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter the first integer: ");
+        int input_1 = userInput.nextInt();
+        System.out.print("Enter the second integer: ");
+        int input_2 = userInput.nextInt();
+
+        // 2.
+
+
+        // 3.
+
+    } // End of method main
+} // End of class RhoDigits
+
