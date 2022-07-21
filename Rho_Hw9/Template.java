e6d2a0d168998f284b09c0b5be6a7b076a05de52
Soobin Rho <soobinrho@gmail.com>
2022-07-21 07:22:23 -0500

    Start HW 9.
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ Rho_Hw9/Template.java
@@ -0,0 +1,21 @@
+// COSC 211
+// Soobin Rho
+// July 21, 2022
+import java.util.Scanner;
+
+public class Rho {
+    /*
+     *
+    */
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter the first integer: ");
+        int input_1 = userInput.nextInt();
+        System.out.print("Enter the second integer: ");
+        int input_2 = userInput.nextInt();
+
+    }
+
+}
+
