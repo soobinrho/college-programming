242dbc8b059507a46cdfac2fb7f931af0349131e
Soobin Rho <soobinrho@gmail.com>
2022-07-31 03:43:40 -0500

    Add FacultyTest.java
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ Rho-Final/StaffTest.java
@@ -0,0 +1,58 @@
+// COSC 211
+// Soobin Rho
+// August 2, 2022
+import java.util.Scanner;
+import java.util.Date;
+
+public class FacultyTest {
+    /*
+     * Testing and debugging
+     * the class Faculty.
+    */
+    public static void main(String[] args) {
+
+        // --------------------------------
+        // Initialize an instance.
+        // --------------------------------
+        Faculty exampleFaculty = new Faculty(
+            "Bean",
+            "Soup",
+            "2000 S. Chilli Factory, 57196",
+            123456789,
+            new Date(1998, 8, 22),
+            100234,
+            new Date(2002, 7, 25),
+            "Adjunct Professor",
+            "MAGI",
+            1200000
+        );
+
+        // --------------------------------
+        // Print the initialized instance.
+        // --------------------------------
+        System.out.println(exampleFaculty);
+        System.out.println();
+
+        // --------------------------------
+        // Test if setters work properly.
+        // --------------------------------
+        exampleFaculty.setFirstName("Fried");
+        exampleFaculty.setLastName("Chicken");
+        exampleFaculty.setAddress("1999 S. Eggs Factory, 57195");
+        exampleFaculty.setSSNum(123123123);
+        exampleFaculty.setBirthDate(new Date(2022, 7, 31));
+        exampleFaculty.setID(432991);
+        exampleFaculty.setStartDate(new Date(2022, 7, 31));
+        exampleFaculty.setTitle("Associate Professor");
+        exampleFaculty.setDept("WIZA");
+        exampleFaculty.setSalary(3750000);
+
+        // --------------------------------
+        // Print the changed values.
+        // --------------------------------
+        System.out.println(exampleFaculty);
+
+    }
+
+}
+
