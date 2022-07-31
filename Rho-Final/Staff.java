0c91135eec2c3dbeeaf8cce14a0bd7e929e8eff6
Soobin Rho <soobinrho@gmail.com>
2022-07-31 03:43:03 -0500

    Complete Staff.java
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ Rho-Final/Staff.java
@@ -0,0 +1,115 @@
+// COSC 211
+// Soobin Rho
+// August 2, 2022
+import java.util.Scanner;
+import java.util.Date;
+
+public class Staff extends Employee {
+    /*
+     * The class Staff is used by the class StudentEmployee
+     * via composition.
+    */
+
+    //------------------------------------
+    // Instance Variables
+    //------------------------------------
+    private String title;        // Job title
+                                 // e.g. "Adjunct Professor"
+
+    private String dept;         // Department
+                                 // e.g. "MAGI"
+
+    private boolean fullTime;    // Whether or not full-time
+                                 // e.g. true
+
+    private double payRate;      // Hourly rate of pay
+                                 // e.g. 15.9
+
+    //------------------------------------
+    // Constructors
+    //------------------------------------
+    public Staff() {
+        this(
+            "Default",
+            "Staff",
+            "2001 S. Summit Ave, 57197",
+            123456789,
+            new Date(1998, 1, 1),
+            100001,
+            new Date(2021, 1, 1),
+            "Stew Staff",
+            "MAGI",
+            true,
+            15.0
+        );
+    }
+
+    public Staff(
+
+        // From super class
+        String firstName,
+        String lastName,
+        String address,
+        int ssNum,
+        Date birthDate,
+        int ID,
+        Date startDate,
+
+        // Solely this class
+        String title,
+        String dept,
+        boolean fullTime,
+        double payRate
+
+    ) {
+
+        // Call the super class's constructor.
+        super(
+            firstName,
+            lastName,
+            address,
+            ssNum,
+            birthDate,
+            ID,
+            startDate
+        );
+
+        // Construct the instance variables.
+        this.title = title;
+        this.dept = dept;
+        this.fullTime = fullTime;
+        this.payRate = payRate;
+
+    }
+
+    //------------------------------------
+    // Setters and Getters
+    //------------------------------------
+    public void setTitle(String title) {this.title = title;}
+    public String getTitle() {return title;}
+
+    public void setDept(String dept) {this.dept = dept;}
+    public String getDept() {return dept;}
+
+    public void setFullTime(boolean fullTime) {this.fullTime = fullTime;}
+    public boolean getFullTime() {return fullTime;}
+
+    public void setpayRate(double payRate) {this.payRate = payRate;}
+    public double getpayRate() {return payRate;}
+
+    //-------------------------------------
+    // toString Method
+    //-------------------------------------
+    public String toString() {
+        String output = String.format(
+            "%s | %s | %s | %s | %,.2f",
+            super.toString(),
+            title,
+            dept,
+            fullTime.toString(),
+            payRate
+        );
+        return output;
+    }
+
+}
