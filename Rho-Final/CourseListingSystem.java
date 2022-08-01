9f2df6080f8b5701e602af53d4479dda41acb6f0
Soobin Rho <soobinrho@gmail.com>
2022-08-01 05:23:18 -0500

    Fix typos
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ Rho-Final/CourseListingSystem.java
@@ -0,0 +1,40 @@
+// COSC 211
+// Soobin Rho
+// August 2, 2022
+
+public class CourseListingSystem {
+    /*
+     * This class is the main controller of the program.
+     * It displays all courses along with
+     * detailed information including who is teaching it
+     * and a list of all students enrolled in the course.
+    */
+
+    // Number of sample data to be created.
+    private static final int NUM_RANDOM_COURSELISTINGS = 12;
+
+    // Name of the file being saved.
+    private static final String SAVE_FILE_PATH = "courseListingSystem.txt";
+
+
+
+
+    //-------------------------------------------
+    // The main method
+    //-------------------------------------------
+    public static void main(String[] args) {
+        ;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
