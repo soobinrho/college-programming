a3d7b7e18975cfb600b7bbbbebb9181d9f1cb66e
Soobin Rho <soobinrho@gmail.com>
2022-08-31 16:59:29 -0500

    Move to final folder
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ fall-2022/c++-programming/hello.cpp
@@ -0,0 +1,14 @@
+// Programmed by: Soobin Rho
+// Hw1: Environment
+// September 2, 2022
+// COSC 226: C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout<<"Hello, World!\n";
+
+}
