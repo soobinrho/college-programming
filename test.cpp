fac194ba122151bc9d6ccc60df7ddf6672737052
Soobin Rho <soobinrho@gmail.com>
2022-09-01 22:37:15 -0500

    Add template c++ file
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ test.cpp
@@ -0,0 +1,23 @@
+// Programmed by: Soobin Rho
+// Hw1: Environment
+// September 2, 2022
+// COSC 226: C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Get user input
+    string input_string;
+    cout << "Enter any sentence: ";
+    cin >> input_string;
+
+    // Echo the input
+    cout << "You've entered " << input_string << ".\n";
+
+    // Return 0 to signal success
+    return 0;
+
+}
