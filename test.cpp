dd06569a13fa1c0beda11e302ca36d67b48183ae
Soobin Rho <soobinrho@gmail.com>
2022-09-02 15:02:33 -0500

    Add std:getline
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- test.cpp
+++ test.cpp
@@ -4,18 +4,29 @@
 // COSC 226: C++
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
 
     // Get user input
-    string input_string = "";
+    string input_string;
+    cout << "Enter any sentence: ";
+    getline(cin, input_string);
+
+    // Echo the input
+    cout << "You've entered \"" << input_string << "\".\n";
+    cout << "\n";
+
+    // Get user input
     cout << "Enter any word: ";
     cin >> input_string;
 
     // Echo the input
-    cout << "You've entered " << input_string << ".\n";
+    cout << "You've entered \"" << input_string << "\".\n";
+    cout << "\n";
 
     // Return 0 to signal success
     return 0;
