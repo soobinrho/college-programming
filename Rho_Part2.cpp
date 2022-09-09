74c80004bd023bb07c8124b12080456fe94963ca
Soobin Rho <soobinrho@gmail.com>
2022-09-09 16:30:15 -0500

    Add regex to Hw2 Part2
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- Rho_Part2.cpp
+++ Rho_Part2.cpp
@@ -4,64 +4,47 @@
 // COSC 226: C++
 // Hw2: Objects, Types, and Values; Computation; Errors
 //
-// This program prompts the user for two integers and then outputs 
-// the smallest, largest, sum, difference, product, and the ratio 
-// of these two.
+// This is a simple calculator that accepts +, -, *, and /.
+// To exit, type any non-numeric character.
+//
+// Example: 
+//     Enter an expression: 2 * 8
+//     16
+//     Enter an expression: -40 - 40
+//     0
 // --------------------------------------------------------------- //
 
 #include <iostream>
 #include <string>
+#include <regex>
 
 using namespace std;
 
 int main()
 {
 
-    // Prompt the user for two integers
-    int val1;
-    int val2;
-    cout << "Enter two integers: ";
-    cin >> val1 >> val2;
+    // A note on the design of this program:
+    // I could've just used cin double, char, and double
+    // individually, but I chose to use a getline and then
+    // use regex to sanitize the input because I wanted to
+    // practice using regex.
+    
+    // Get user input
+    string input_string;
+    cout << "Enter an expression: ";
+    getline(cin, input_string);
 
-    // Get the smallest and the largest
-    int smallest;
-    int largest;
-    if (val1 < val2) {
-        smallest = val1;
-        largest = val2;
-    }
-    else if (val1 > val2) {
-        smallest = val2;
-        largest = val1;
-    }
-    else if (val1 == val2) {
-        smallest = val1;
-        largest = val1;
-    }
-
-    // Note: Above, I could've used else instead of else if, 
-    // but I did this to make it easier to understand.
-
-    // Get the sum
-    int sum = val1 + val2;
-
-    // Get the difference
-    int difference = val1 - val2;
-
-    // Get the product
-    int product = val1 * val2;
-
-    // Get the ratio
-    int quotient = val1 / val2;
+    // Sanitize the input
+    //string pattern_valid = R"([\+|-|\*|/]hello)";
+    string pattern_valid = R"(\s*([-]{0,1}\d+))"     // e.g. 2
+                           R"(\s*([\+|-|\*|/]))"     // e.g. *
+                           R"(\s*([-]{0,1}\d+))";    // e.g. 8
+    bool is_valid = regex_match(input_string, regex(pattern_valid));
 
     // Print the results
     cout << "\n"
-         << "smallest = " << smallest << "\n"
-         << "largest = " << largest << "\n"
-         << "sum = " << sum << "\n"
-         << "difference = " << difference << "\n"
-         << "product = " << product << "\n"
-         << "ratio = " << quotient << "\n";
+         << input_string << "\n"
+         << "is_valid = " << is_valid << "\n";
 
     // Return 0 to signal success
     return 0;
