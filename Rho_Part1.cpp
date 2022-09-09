2159cae65b31b0e524b8a3c2e461332429deb3c8
Soobin Rho <soobinrho@gmail.com>
2022-09-09 15:11:09 -0500

    Change comment section formattng for template file
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- Rho_Part1.cpp
+++ Rho_Part1.cpp
@@ -1,9 +1,10 @@
 // ---------------------------------------------------- //
-// Programmed by: Soobin Rho
+// Soobin Rho
 // September 14, 2022
 // COSC 226: C++
 // Hw2: Objects, Types, and Values; Computation; Errors
 //
+// Part 1
 // This program prompts the user for two integers
 // and then outputs the smallest, largest, sum,
 // difference, product, and the ratio of these two.
@@ -17,24 +18,28 @@ using namespace std;
 int main()
 {
 
-    // Get user input
-    string input_string;
-    cout << "Enter any word: ";
-    cin >> input_string;
-
-    // Echo the input
-    cout << "You've entered \"" << input_string << "\".\n";
-    cout << "\n";
-
-    // Get user input
-    cout << "Enter any sentence: ";
-    std::getline(cin, input_string);
-    std::getline(cin, input_string);
+    // Prompt the user for two integers
+    int val1;
+    int val2;
+    cout << "Enter two integers: ";
+    cin >> val1 >> val2;
 
 
-    // Echo the input
-    cout << "You've entered \"" << input_string << "\".\n";
-    cout << "\n";
+    cout << val1 << val2;
+
+    /* // Echo the input */
+    /* cout << "You've entered \"" << input_string << "\".\n"; */
+    /* cout << "\n"; */
+
+    /* // Get user input */
+    /* cout << "Enter any sentence: "; */
+    /* std::getline(cin, input_string); */
+    /* std::getline(cin, input_string); */
+
+
+    /* // Echo the input */
+    /* cout << "You've entered \"" << input_string << "\".\n"; */
+    /* cout << "\n"; */
 
 
     // Return 0 to signal success
--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
-// Programmed by: Soobin Rho
-// Hw1: Environment
-// September 2, 2022
+// ---------------------------------------------------- //
+// Soobin Rho
+// September 14, 2022
 // COSC 226: C++
+// Hw2: Objects, Types, and Values; Computation; Errors
+//
+// Part 1
+// This program prompts the user for two integers
+// and then outputs the smallest, largest, sum,
+// difference, product, and the ratio of these two.
+// ---------------------------------------------------- //
 
 #include <iostream>
 #include <string>
@@ -11,24 +18,28 @@ using namespace std;
 int main()
 {
 
-    // Get user input
-    string input_string;
-    cout << "Enter any word: ";
-    cin >> input_string;
-
-    // Echo the input
-    cout << "You've entered \"" << input_string << "\".\n";
-    cout << "\n";
-
-    // Get user input
-    cout << "Enter any sentence: ";
-    std::getline(cin, input_string);
-    std::getline(cin, input_string);
+    // Prompt the user for two integers
+    int val1;
+    int val2;
+    cout << "Enter two integers: ";
+    cin >> val1 >> val2;
 
 
-    // Echo the input
-    cout << "You've entered \"" << input_string << "\".\n";
-    cout << "\n";
+    cout << val1 << val2;
+
+    /* // Echo the input */
+    /* cout << "You've entered \"" << input_string << "\".\n"; */
+    /* cout << "\n"; */
+
+    /* // Get user input */
+    /* cout << "Enter any sentence: "; */
+    /* std::getline(cin, input_string); */
+    /* std::getline(cin, input_string); */
+
+
+    /* // Echo the input */
+    /* cout << "You've entered \"" << input_string << "\".\n"; */
+    /* cout << "\n"; */
 
 
     // Return 0 to signal success
