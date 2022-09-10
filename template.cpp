801b447984c26e2d4158726afb16b7fab618e40d
Soobin Rho <soobinrho@gmail.com>
2022-09-09 20:48:05 -0500

    Move to final folder
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- template.cpp
+++ template.cpp
@@ -4,59 +4,96 @@
 // COSC 226: C++
 // Hw2: Objects, Types, and Values; Computation; Errors
 //
-// This program prompts the user for two integers and then outputs 
-// the smallest, largest, sum, difference, product, and the ratio 
-// of these two.
+// This is a simple calculator that accepts +, -, *, and /.
+// To exit, type any non-numeric character.
+//
+// Example: 
+//     Enter an expression: 2 * 8
+//     16
+//     Enter an expression: -40 - 40
+//     0
 // --------------------------------------------------------------- //
 
 #include <iostream>
 #include <string>
+#include <regex>
 
 using namespace std;
 
 int main() {
 
-    // Prompt the user for two integers
-    int val1;
-    int val2;
-    cout << "Enter two integers: ";
-    cin >> val1 >> val2;
+    // ----------------------------------------- //
+    // Two possibilities for inputs:
+    // 1. Valid inputs (e.g. -40 - 40)
+    // 2. Exit code (Contains non-numeric value)
+    // ----------------------------------------- //
+    smatch matches;
+    string pattern_valid = R"(\s*([\-]?\d+\.?\d*))"     // e.g. 2
+                           R"(\s*([\+|\-|\*|/]))"       // e.g. *
+                           R"(\s*([\-]?\d+\.?\d*)\s*)";    // e.g. 8
 
-    // Print the smallest and the largest
-    int smallest;
-    int largest;
-    if (val1 < val2) {
-        val1 = smallest;
-        val2 = largest;
+    // Loop until the user inputs a non-numeric value
+    string input;
+    double result;
+    bool is_valid = true;
+    while (is_valid) {
+
+        // Get user input
+        cout << "Enter an expression: ";
+        getline(cin, input);
+        is_valid = regex_match(input, matches, regex(pattern_valid));
+
+        // If the input contains any non-numeric value, exit
+        if (!is_valid) break;
+
+        // ----------------------------------------- //
+        // Structure of matches:
+        // matches[0] -> the whole match
+        // matches[1] -> num_1        (e.g. 2)
+        // matches[2] -> operation    (e.g. *)
+        // matches[3] -> num_2        (e.g. 8)
+        // ----------------------------------------- //
+
+        // Convert the input string into num_1, operation, and num_2
+        double num_1 = stod(matches[1].str());
+        char operation = matches[2].str()[0];
+        double num_2 = stod(matches[3].str());
+
+        // ----------------------------------------- //
+        // Print the result
+        // ----------------------------------------- //
+        // Side note:
+        // cout << std::scientific is the default, but I could use
+        // cout << std::fixed if I didn't want the scientific notation.
+        switch (operation) {
+
+            case '+':
+                result = num_1 + num_2;
+                cout << "The sum of " << num_1 << " and " << num_2
+                     << " is " << result << "\n\n";
+                break;
+
+            case '-':
+                result = num_1 - num_2;
+                cout << "The difference of " << num_1 << " and " << num_2
+                     << " is " << result << "\n\n";
+                break;
+
+            case '*':
+                result = num_1 * num_2;
+                cout << "The product of " << num_1 << " and " << num_2
+                     << " is " << result << "\n\n";
+                break;
+
+            case '/':
+                result = num_1 / num_2;
+                cout << "The ratio of " << num_1 << " and " << num_2
+                     << " is " << result << "\n\n";
+                break;
+
+        }
+
     }
-    else if (val1 > val2) {
-        val1 = largest;
-        val2 = smallest;
-    }
-    else if (val1 == val2) {
-        val1 = smallest;
-        val1 = largest;
-    }
-
-    // Note: I could've used else instead of else if, 
-    // but I chose to be verbose to make it easier to understand.
-
-    cout << val1 << val2;
-
-    /* // Echo the input */
-    /* cout << "You've entered \"" << input_string << "\".\n"; */
-    /* cout << "\n"; */
-
-    /* // Get user input */
-    /* cout << "Enter any sentence: "; */
-    /* std::getline(cin, input_string); */
-    /* std::getline(cin, input_string); */
-
-
-    /* // Echo the input */
-    /* cout << "You've entered \"" << input_string << "\".\n"; */
-    /* cout << "\n"; */
-
 
     // Return 0 to signal success
     return 0;
