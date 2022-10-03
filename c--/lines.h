8158ed7783fbc88512cd38eed4a9a80e5984c510
Soobin Rho <soobinrho@gmail.com>
2022-10-03 13:24:24 -0500

    Add all header files
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ c--/lines.h
@@ -0,0 +1,27 @@
+/*
+ *    COSC 226: C++ Programming
+ *    Marcus Naess, Shannon McCormick, Soobin Rho
+ *    Soobin Rho
+ *    October 3, 2022
+ *    In-class acitivity
+ */ 
+
+#include <iostream>    // Required for cout
+#include <vector>      // Required for vector<int>
+#include "points.h"
+
+using namespace std;
+
+vector<int> reverseIntVectorA(const vector<int>& vectorInput);
+void reverseIntVectorB(vector<int>& vectorInput);
+void printVector(const vector<int>& vectorInput);
+
+int main() {
+    /*
+     *    An exercise for reversing int verctors
+     */
+
+
+    // Return 0 to signal success
+    return 0;
+}
