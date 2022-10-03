aeeded129257963fc3c912644386d8aa25e30106
Soobin Rho <soobinrho@gmail.com>
2022-10-03 13:40:49 -0500

    Fix typo (Remove name twice)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- c--/lines.h
+++ c--/lines.h
@@ -1,7 +1,6 @@
 /*
  *    COSC 226: C++ Programming
  *    Marcus Naess, Shannon McCormick, Soobin Rho
- *    Soobin Rho
  *    October 3, 2022
  *    In-class acitivity
  */ 
--- c--/points.h
+++ c--/points.h
@@ -1,7 +1,6 @@
 /*
  *    COSC 226: C++ Programming
  *    Marcus Naess, Shannon McCormick, Soobin Rho
- *    Soobin Rho
  *    October 3, 2022
  *    In-class acitivity
  */ 
@@ -10,3 +9,5 @@
 #include <vector>      // Required for vector<int>
 
 using namespace std;
+
+
