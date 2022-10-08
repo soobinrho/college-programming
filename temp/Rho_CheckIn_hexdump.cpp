a126572d35b1e1361171106a9f2379c5c9ca698c
Soobin Rho <soobinrho@gmail.com>
2022-10-08 00:31:00 -0500

    Complete hexdump function
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- temp/Rho_CheckIn_hexdump.cpp
+++ temp/Rho_CheckIn_hexdump.cpp
@@ -16,51 +16,62 @@
 
 using namespace std;
 
-void hexdump(istream& ist);
-void hexdumpAnotherWay(istream& ist);
+void hexdump(string fileName);
+void hexdumpAnotherWay(string fileName);
 void throwInvalidInput();
 
 int main() {
-    /*
-     *    Read a file and print its hex values.
-     */
+    // ---------------------------------------------------
+    // 1. Oepn the test file in the binary mode
+    // ---------------------------------------------------
+    const string TESTFILENAME = "test.txt";
 
     // ---------------------------------------------------
-    // 1. Read the file in binary
+    // 2. Read the file and print in hexadecimal
     // ---------------------------------------------------
-    const string FILENAME = "test.txt";  // NOTE: Change to user input
-    ifstream ifs {FILENAME,ios_base::binary};
-    if (!ifs) throwInvalidInput();
-
-
-    /* ifs.seekg(0, ifs.end); */
-    /* int length = ifs.ge; */
-
-
-
-    hexdumpAnotherWay(ifs);
-
-    ifs.close();
-
-    // ---------------------------------------------------
-    //  . Convert to hex and print
-    // ---------------------------------------------------
-
-
+    hexdump(TESTFILENAME);
 
     // Return 0 to signal success
     return 0;
 }
 
-void hexdump(istream& ist){
+void hexdump(string fileName) {
     /*
-     *    A
+     *    A function for reading a file in hexadecimal.
      */
 
+    ifstream ifs {fileName,ios_base::binary};
+    if (!ifs) throwInvalidInput();
 
+    cout<<setfill('0')
+        <<hex;
+
+    int count = 0;
+    int countTotalBytes = 0;
+
+    int intIn;
+    void* address = &intIn;
+    while (ifs.read(static_cast<char*>(address),sizeof(char))) {
+        // Print the number of total bytes
+        // e.g. 00000010
+        if (count%16==0 || count==0) {
+            cout<<setw(8)<<countTotalBytes<<' ';
+        }
+
+        // Print the hexadeximals
+        // e.g. 49 6c 6f 76 65 70 61 73 74 61 77 69 74 68 74 6f
+        // This, by the way, means "Ilovepastawithto[matosauce]"
+        cout<<setw(2)<<intIn<<' ';
+
+        ++count;
+        if (count%16==0) {
+            cout<<'\n';
+            countTotalBytes+=16;
+        }
+    }
 }
 
-void hexdumpAnotherWay(istream& ist) {
+void hexdumpAnotherWay(string fileName) {
     /*
      *    Actually, before writing the previous hexdump function,
      *    I wrote this function for better understanding of the problem.
@@ -73,13 +84,16 @@ void hexdumpAnotherWay(istream& ist) {
      *    because it doesn't use any pointer unlike the previous function.
      */
 
+    ifstream ifs {fileName,ios_base::binary};
+    if (!ifs) throwInvalidInput();
+
     cout<<setfill('0')
         <<hex;
 
     int count = 0;
     int countTotalBytes = 0;
 
-    for (char charIn; ist.get(charIn);) {
+    for (char charIn; ifs.get(charIn);) {
         // Print the number of total bytes
         // e.g. 00000010
         if (count%16==0 || count==0) {
