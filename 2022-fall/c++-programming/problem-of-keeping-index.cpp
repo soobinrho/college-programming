f511300b4c31ef1bfbb8a798003f5d86814bdea6
Soobin Rho <soobinrho@gmail.com>
2022-12-08 17:20:23 -0600

    feat: start of problem of keeping index (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ 2022-fall/c++-programming/problem-of-keeping-index.cpp
@@ -0,0 +1,4 @@
+// How to sort just using vector but retain the index order?
+//
+// sort using one characteristic. If two or more values
+// have the same value, then sort again using another characteristic
