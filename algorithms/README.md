4c33ede68336f17f17423f905b2b663aa8330fd1
Soobin Rho <soobinrho@gmail.com>
2023-01-13 20:34:23 -0600

    docs: add compile instruction for algorithms
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ algorithms/README.md
@@ -0,0 +1,18 @@
+# Algorithms
+These are the problem sets I really liked from books or LeetCode.
+All of them contain test cases using `catch2`.
+
+<br>
+
+***How to Compile and Run***
+```bash
+# Compile example
+g++ -o _ palidnromes-inside-substrings.cpp catch2/*
+
+# Run
+./_ -s
+```
+
+<br>
+<br>
+<br>
--- algorithms/palindromes-inside-substrings.cpp
+++ algorithms/palindromes-inside-substrings.cpp
@@ -7,16 +7,31 @@
 using namespace std;
 
 namespace {
-  string getNearbyPalindrome(string str) {
+  string getNearbyPalindrome(string& str) {
+    /*
+     *   This is used by getLongestPalindrome function for finding
+     *   a nearby palindrome given its 
+     */
+
     return str;
   }
 
+
   string getLongestPalindrome(string inputStr) {
-    return "inputStr";
+    /*
+     *   A function that finds the longest palindrome inside substrings.
+     *   For example, an input of "abcdeffedcbabccba" first finds all
+     *   palindromes including "abcdeffedcba" and "abccba". It then
+     *   returns the longest of all.
+     */
+
+    return inputStr;
   }
+
+  // TODO: move definition of getNearbyPalindrome to here
 }
 
-TEST_CASE("Palindromes inside a vector","[palindrome]") {
+TEST_CASE("Palindromes Inside Substrings","[palindrome]") {
   SECTION("Testing correct cases") {
     REQUIRE(getLongestPalindrome("eeeeee")=="eeeeee");
     CHECK(getLongestPalindrome("abccef")=="cc");
@@ -24,6 +39,7 @@ TEST_CASE("Palindromes inside a vector","[palindrome]") {
     CHECK(getLongestPalindrome("abcba")=="abcba");
     CHECK(getLongestPalindrome("abccdefghiihgfed")=="defghiihgfed");
     CHECK(getLongestPalindrome("abccdefghihgfed")=="defghihgfed");
+    CHECK(getLongestPalindrome("abcdeffedcbabccba")=="abcdeffedcba");
   }
 
   SECTION("Testing exceptions") {
