98975ccc14fbd5b9dd8112d159bc0d18a8cd56a4
Soobin Rho <soobinrho@gmail.com>
2023-01-14 03:41:25 -0600

    feat: palindromes inside substrings
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- algorithms/palindromes-inside-substrings.cpp
+++ algorithms/palindromes-inside-substrings.cpp
@@ -2,53 +2,100 @@
 #include "catch2/catch_amalgamated.hpp"
 
 #include <string>
-#include <stdexcept>
+#include <sstream>
+#include <iostream>
 
 using namespace std;
 
 namespace {
-  string getNearbyPalindrome(string& str) {
+  string getNearbyPalindrome(const string& str,
+                             int ptrLeft,
+                             int ptrRight) {
     /*
      *   This is used by getLongestPalindrome function for finding
-     *   a nearby palindrome given its 
+     *   a nearby palindrome given its left index (ptrLeft)
+     *   and right index (ptrRight).
      */
 
-    return str;
-  }
+    // Expand from the initial pointers and find how far the
+    // palindrome goes.
+    const int strLength = str.length();
+    while (ptrLeft-1>=0 && ptrRight+1<strLength &&
+           str[ptrLeft-1]==str[ptrRight+1]) {
+      --ptrLeft;
+      ++ptrRight;
+      // DEBUG
+      cout<<"[DEBUG] "<<str.substr(ptrLeft,ptrRight-ptrLeft+1)<<"\n";
+    }
 
+    const string localPalindrome = str.substr(ptrLeft,ptrRight-ptrLeft+1);
+
+    return localPalindrome;
+  }
 
   string getLongestPalindrome(string inputStr) {
     /*
      *   A function that finds the longest palindrome inside substrings.
      *   For example, an input of "abcdeffedcbabccba" first finds all
      *   palindromes including "abcdeffedcba" and "abccba". It then
-     *   returns the longest of all.
+     *   returns the longest of all. Also, if two palindromes have the
+     *   same length, the function returns the one with the
+     *   greater lexicographical value.
      */
 
-    return inputStr;
+    // TWO POSSIBILITIES FOR PALINDROMES
+    // A. The character at the index is the same as the character at
+    //    index+1.
+    // B. The character at index-1 is the same as index+1.
+    string longestPalindrome;
+    const int strLength = inputStr.length();
+    for (int i=0; i<strLength; ++i) {
+      const int char_here = inputStr[i];
+      const int char_before = i>0 ? inputStr[i-1] : '!';
+      const int char_after = i<strLength-1 ? inputStr[i+1] : '@';
+
+      // A. The character at the index is the same as the character at
+      //    index+1.
+      if (char_here==char_after) {
+        const string localPalindrome = getNearbyPalindrome(inputStr,i,i+1);
+
+        if (localPalindrome.length()>longestPalindrome.length())
+          longestPalindrome=localPalindrome;
+
+        else if (localPalindrome.length()==longestPalindrome.length() &&
+                 localPalindrome>longestPalindrome)
+          longestPalindrome=localPalindrome;
+      }
+
+      // B. The character at index-1 is the same as index+1.
+      if (char_before==char_after) {
+        const string localPalindrome = getNearbyPalindrome(inputStr,i-1,i+1);
+
+        if (localPalindrome.length()>longestPalindrome.length())
+          longestPalindrome=localPalindrome;
+
+        else if (localPalindrome.length()==longestPalindrome.length() &&
+                 localPalindrome>longestPalindrome)
+          longestPalindrome=localPalindrome;
+      }
+    }
+
+    return longestPalindrome;
   }
 
-  // TODO: move definition of getNearbyPalindrome to here
+  // TODO: move definition of getNearbyPalindrome to here.
 }
 
 TEST_CASE("Palindromes Inside Substrings","[core]") {
   SECTION("Testing correct cases") {
-    REQUIRE(getLongestPalindrome("eeeeee")=="eeeeee");
-    CHECK(getLongestPalindrome("abccef")=="cc");
+    CHECK(getLongestPalindrome("eeeeee")=="eeeeee");
+    REQUIRE(getLongestPalindrome("abccef")=="cc");
     CHECK(getLongestPalindrome("abccba")=="abccba");
     CHECK(getLongestPalindrome("abcba")=="abcba");
     CHECK(getLongestPalindrome("abccdefghiihgfed")=="defghiihgfed");
     CHECK(getLongestPalindrome("abccdefghihgfed")=="defghihgfed");
     CHECK(getLongestPalindrome("abcdeffedcbabccba")=="abcdeffedcba");
     CHECK(getLongestPalindrome("abcdeffedcbabcba")=="abcdeffedcba");
-  }
-
-  SECTION("Testing exceptions") {
-    string message = "Invalid input.";
-    CHECK_THROWS_MATCHES(
-      getLongestPalindrome("111"),
-      logic_error,
-      Catch::Matchers::ExceptionMessageMatcher(message)
-    );
+    CHECK(getLongestPalindrome("aaaaaabbbaaaaaa")=="aaaaaabbbaaaaaa");
   }
 }
