7b7f7da4d3021cbbac1519342c06347f6aead8b3
Soobin Rho <soobinrho@gmail.com>
2023-01-13 19:29:58 -0600

    chore: it turns out there's no need for a header file
    In this case, every declaration and definition can be
    put inside the test file.
    
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- algorithms/palindromes-inside-vector.cpp
+++ algorithms/palindromes-inside-vector.cpp
@@ -1,4 +1,7 @@
-#include "palindromes-inside-vector.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch_amalgamated.hpp"
+
+#include <string>
 
 using namespace std;
 
@@ -8,4 +11,14 @@ string getNearbyPalindrome(string str) {
 
 string getLongestPalindrome(string inputStr) {
   return inputStr;
+}
+
+TEST_CASE("Palindromes inside a vector","[palindrome]") {
+  SECTION("Testing all palindrome cases") {
+    REQUIRE(getLongestPalindrome("abccef")=="cc");
+    REQUIRE(getLongestPalindrome("abccba")=="abccba");
+    REQUIRE(getLongestPalindrome("abcba")=="abcba");
+    REQUIRE(getLongestPalindrome("abccdefghiihgfed")=="defghiihgfed");
+    REQUIRE(getLongestPalindrome("abccdefghihgfed")=="defghihgfed");
+  }
 }
