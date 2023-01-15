4fac3617f32ef6c32119235f958b5eabda1f9784
Soobin Rho <soobinrho@gmail.com>
2023-01-15 00:37:32 -0600

    refactor: more concise syntax
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- algorithms/Codeforces/4A-watermelon.cpp
+++ algorithms/Codeforces/4A-watermelon.cpp
@@ -3,23 +3,16 @@
 
 using namespace std;
 
-string checkEvenWatermelon(int inputInt) {
+int main() {
   /*
    *   Check if a watermelon can be divided into two parts.
    *   The condition is that both parts have to be an even number.
-   *
    */
+
+  int inputInt;
+  cin>>inputInt;
   const string answer = inputInt%2==0 ? "YES" : "NO";
 
-  if (inputInt==2) return "NO";
-  else return answer;
-}
-
-int main() {
-  int buffer;
-  cin>>buffer;
-  cout<<checkEvenWatermelon(buffer);
-
-  // Return 0 to signal success
-  return 0;
+  if (inputInt==2) cout<<"NO";
+  else cout<<answer;
 }
