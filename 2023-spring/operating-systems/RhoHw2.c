8e2cd4b7d93a53254cfd17c4a65c680ac7cca82c
Soobin Rho <soobinrho@gmail.com>
2023-02-12 17:43:04 -0600

    feat: start of hw2 (wip)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ 2023-spring/operating-systems/RhoHw2.c
@@ -0,0 +1,24 @@
+/*
+ *    COSC 310: Operating Systems
+ *    Soobin Rho
+ *    February 12, 2022
+ *    Hw2: Two Arrays
+ */ 
+
+/*
+Create a C program that reads in two arrays of integers (of the
+same size) from stdIn (prompt the user).  You will create and
+initialize them in the mainline routine.  You will then add them
+together, storing the result in another array.  Write a
+separate function to print an array (you will print all
+three arrays with separate calls to the print function)
+and a separate function to add the arrays.  Each routine
+(mainline or function) should be in a separate file.
+This program will follow the ANSI-C standard and must compile with GCC on the system you chose to work on.   You may use any editor.
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("output\n");
+}
