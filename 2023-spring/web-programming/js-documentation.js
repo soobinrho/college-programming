87e8826bb0f561bc6cf919dd31228aa6a8b4688f
Soobin Rho <soobinrho@gmail.com>
2023-03-18 20:09:05 -0500

    feat: start of BASICS of JavaScript (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ 2023-spring/web-programming/js-documentation.js
@@ -0,0 +1,28 @@
+// BASICS OF JavaScript
+// This file based on Microsoft's JavaScript tutorials.
+// Source:
+//   https://learn.microsoft.com/en-us/shows/beginners-series-to-javascript/
+
+// ====================
+// How to run this file
+// ====================
+// node js-documentation.js
+
+// ======
+// String
+// ======
+// const sub = "string substitution";
+// console.log(`This is a ${sub}`);
+
+// ============
+// VS Code TipsTODO Highlight (Extension)
+// ============
+// # TODO Highlight (Extension)
+// TODO: and FIXME: become hightlighted.
+//
+// # Tips
+// - Use the vim extension.
+// - Ctrl + ` : Open / close a shell
+// - Ctrl + x : Open the extension tab
+// - Ctrl + tab : Switch to the next tab
+// - Ctrl + shift + tab : Switch to the next tab (reverse)
