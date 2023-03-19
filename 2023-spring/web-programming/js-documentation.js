375d1ac4cf2133f88856d86fb5834d6523f04a7a
Soobin Rho <soobinrho@gmail.com>
2023-03-18 20:17:22 -0500

    docs: explanation on difference between let and var
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- 2023-spring/web-programming/js-documentation.js
+++ 2023-spring/web-programming/js-documentation.js
@@ -14,9 +14,9 @@
 // const sub = "string substitution";
 // console.log(`This is a ${sub}`);
 
-// ============
+// ======================================
 // VS Code TipsTODO Highlight (Extension)
-// ============
+// ======================================
 // # TODO Highlight (Extension)
 // TODO: and FIXME: become hightlighted.
 //
@@ -24,5 +24,14 @@
 // - Use the vim extension.
 // - Ctrl + ` : Open / close a shell
 // - Ctrl + x : Open the extension tab
+// - Ctrl + w : Close the tab
+// - Ctrl + , : Open the settings
 // - Ctrl + tab : Switch to the next tab
 // - Ctrl + shift + tab : Switch to the next tab (reverse)
+
+// For declaring variables, generally use two ways, but 
+// use const as much as you can.
+//   let a = 1;
+//   const b = 2;
+// * var is available as well, but it's dangerous as it makes that variable
+//   available even above the declaration line, unlike let.
