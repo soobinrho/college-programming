b01d5b686b051cc75c422ac4e6bba8eec117ff6b
Soobin Rho <soobinrho@gmail.com>
2023-03-25 18:18:42 -0500

    feat: start of Lab 5 (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ 2023-spring/operating-systems/RhoLab5/RhoLab5.c
@@ -0,0 +1,77 @@
+/*
+ *    COSC 310: Operating Systems
+ *    Soobin Rho
+ *    March 25, 2022
+ *    Lab 5: Virtual Memory Simulator
+ */ 
+
+#include <stdio.h>
+
+void addArrays(int* arrSum, int* arr1, int* arr2, int SIZE) {
+  int i;
+  for (i=0;i<SIZE;++i) {
+    arrSum[i] = arr1[i]+arr2[i];
+  }
+}
+
+void printArray(int* arr, int SIZE) {
+  int i;
+  for (i=0;i<SIZE;++i) {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  // ------------------------------------------------------------------
+  // Get user input
+  // ------------------------------------------------------------------
+  printf("Enter the size of each array: ");
+  int SIZE;
+  scanf("%d",&SIZE);
+
+  printf("\nType two integer arrays of size %d.\n",SIZE);
+  printf("Example:\n");
+  printf("  1 3 2 5 2\n");
+  printf("  5 9 6 8 4\n\n");
+
+  printf("ARRAY 1\n");
+  int arr1[SIZE];
+  int i;
+  for (i=0;i<SIZE;++i) {
+    printf("Enter an integer (%d out of %d): ",i+1,SIZE);
+    scanf("%d",&arr1[i]);
+  }
+
+  printf("\nARRAY 2\n");
+  int arr2[SIZE];
+  for (i=0;i<SIZE;++i) {
+    printf("Enter an integer (%d out of %d): ",i+1,SIZE);
+    scanf("%d",&arr2[i]);
+  }
+
+  // ------------------------------------------------------------------
+  // Create a new array with the sum of both arrays
+  // ------------------------------------------------------------------
+  int arrSum[SIZE];
+  addArrays(arrSum,arr1,arr2,SIZE);
+
+  // ------------------------------------------------------------------
+  // Print the arrays
+  // ------------------------------------------------------------------
+  printf("\nRESULTS\n");
+  printf("=======\n");
+
+  printf("\nARRAY 1\n");
+  printArray(arr1,SIZE);
+
+  printf("\nARRAY 2\n");
+  printArray(arr2,SIZE);
+
+  printf("\nSUM ARRAY\n");
+  printArray(arrSum,SIZE);
+
+  return 0;
+}
+
+
