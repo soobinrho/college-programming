b085a352d56aadce501b87b0dc736a318fec5358
Soobin Rho <soobinrho@gmail.com>
2023-03-25 21:02:08 -0500

    fix: regex implementation for decode function (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- 2023-spring/operating-systems/RhoLab5/RhoLab5.c
+++ 2023-spring/operating-systems/RhoLab5/RhoLab5.c
@@ -12,17 +12,25 @@
 
 int main() {
 
-  char DUMP[] = "dump\n";
-  char VERBOSE_ON[] = "verbose_on\n";
-  char VERBOSE_OFF[] = "verbose_off\n";
-  char TEXTBOOK[] = "textbook\n";
+  // These are all accepted commands from user input.
   char HELP_1[] = "help\n";
   char HELP_2[] = "?\n";
   char QUIT_1[] = "quit\n";
   char QUIT_2[] = "q\n";
+  char TEXTBOOK_1[] = "textbook\n";
+  char TEXTBOOK_2[] = "textbook\n";
+  char VERBOSE_ON[] = "verbose_on\n";
+  char VERBOSE_OFF[] = "verbose_off\n";
+  char DUMP[] = "dump\n";
 
-  regex_t DECODE;
-  (void) regcomp(&DECODE,"decode[:space:]+[:digit:]+",0);
+  // The match array has two member because match[0] is always
+  // the entire match, while match[1] is the first subgroup.
+  // Since the DECODE regex has one subgroup - i.e. `([:digit:]+)` -
+  // SIZE_GROUPS must be 1 entire match + 1 subgroup = 2.
+  size_t SIZE_GROUPS = 2;
+  regmatch_t match[SIZE_GROUPS];
+  regex_t DECODE_REGEX;
+  (void) regcomp(&DECODE_REGEX,"decode[:space:]+([:digit:]+)",0);
 
   // ------------------------------------------------------------------
   // Get user input.
@@ -39,28 +47,38 @@ int main() {
     // 11 characters + 1 null character (\0).
     printf("Enter a command: ");
     getline(&command,&commandLength,stdin);
-    printf("INPUT: %s \n",command);
+
+    // -----------------------------------------------------------------
+    // Execute commands depending on the user input.
+    // -----------------------------------------------------------------
     if (strcmp(command,QUIT_1)==0 || strcmp(command,QUIT_2)==0) {
       isTerminated = true;
     }
     else if (strcmp(command,HELP_1)==0 || strcmp(command,HELP_2)==0) {
-      printf("HELP FUNCTION");
+      printf("HELP FUNCTION\n");
+    }
+    else if (strcmp(command,TEXTBOOK_1)==0 || strcmp(command,TEXTBOOK_2)==0) {
+      printf("TEXTBOOK FUNCTION\n");
     }
     else if (strcmp(command,VERBOSE_ON)==0) {
-      printf("VERBOSE_ON FUNCTION");
+      printf("VERBOSE_ON FUNCTION\n");
     }
     else if (strcmp(command,VERBOSE_OFF)==0) {
-      printf("VERBOSE_OFF FUNCTION");
+      printf("VERBOSE_OFF FUNCTION\n");
     }
-
-
+    else if (strcmp(command,DUMP)==0) {
+      printf("DUMP FUNCTION\n");
+    }    
+    else if (regexec(&DECODE_REGEX,command,SIZE_GROUPS,match,0)==0) {
+      printf("DECODE FUNCTION with match[1]\n");
+    }
     else {
-      printf("NOPE %d\n",strcmp(command,QUIT_1));
+      printf("[ERROR] Invalid input. Enter help (or ?).\n");
+      printf("SUBGROUP: %s\n",command);
     }
-
   }
 
-
+  // Return 0 to indicate success.
   return 0;
 }
 
