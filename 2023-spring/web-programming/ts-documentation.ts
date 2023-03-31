3ec0fa07efeb3fdbb9a5d70b6bd53ae4886dd726
Soobin Rho <soobinrho@gmail.com>
2023-03-31 00:00:54 -0500

    docs: why use readonly
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- 2023-spring/web-programming/ts-documentation.ts
+++ 2023-spring/web-programming/ts-documentation.ts
@@ -25,3 +25,40 @@ Source:
 // * Prefer `interface` over `type`, unless we need the features
 //   of `type` specifically.
 // * Likewise, prefer `interface` over `class`.
+
+// ===========================
+// Specific features of `type`
+// ===========================
+// type RegistrationStatus = "registered" | "waitlisted" | "none";
+// type StatusNumber = 1 | 3 | 5;
+// type InputType = string | number;
+//
+// * This is called unions.
+
+// ==========================
+// Unions for parameter types
+// ==========================
+// function printString(str: string | string[]) {
+//   console.log(str);
+// }
+
+// ===========================
+// Find the type of a variable
+// ===========================
+// typeof name === "string"
+// typeof isName === "boolean"
+
+// =========================
+// How to declare `readonly`
+// =========================
+// interface Person {
+//   readonly name: string;
+// }
+
+// ===================
+// Why use `readonly`?
+// ===================
+// const arr = [0, 1, 2];
+//
+// * `arr.push(3)` will give an error, but `arr[0] = -1;` will
+//   still work, which is why we need `readonly`.
