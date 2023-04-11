a2f7875f464dfb484ab69492a047c0939571a84f
Soobin Rho <soobinrho@gmail.com>
2023-04-10 20:15:05 -0500

    feat: storeFile outlines (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- 2023-spring/operating-systems/RhoLab6-file-system-simulator/RhoLab6-file-system-simulator.cpp
+++ 2023-spring/operating-systems/RhoLab6-file-system-simulator/RhoLab6-file-system-simulator.cpp
@@ -10,6 +10,7 @@ Lab 6: File System Simulator
 // For learning purposes, I've written down notes from
 // "Operating Systems and Middleware" by Max Hailperin.
 
+#include <cmath>
 #include <string>
 #include <vector>
 #include <memory>
@@ -30,11 +31,11 @@ using namespace std;
 // --------------------------------------------------------------------
 const int BLOCK_SIZE = 1024;
 const int TOTAL_BLOCKS = 200;
+const int TOTAL_BLOCKS_80_PERCENT = std::floor(TOTAL_BLOCKS*0.8);
 const int TOTAL_SIZE = BLOCK_SIZE*TOTAL_BLOCKS;
 
 struct FileSystemContiguous {
-    vector<int> whichFileThisIsMappedTo;
-    vector<string> whichFileName;
+    vector<string> whichFileThisIsMappedTo;
 
     FileSystemContiguous () {};
     ~FileSystemContiguous () {};
@@ -67,18 +68,77 @@ struct FileSystemLinkedListFAT {
 // --------------------------------------------------------------------
 // Declarations for helper functions
 // --------------------------------------------------------------------
-void store (FileSystemContiguous fileSystem, string fileName, int numBytes) {
+void accessFile (FileSystemContiguous fileSystem, string fileName) {
+
+}
+
+void _runDefragmentation (FileSystemContiguous fileSystem) {
+
+}
+
+int _getAvailableBlock (FileSystemContiguous fileSystem, int howManyBlocks) {
+    // Traverse through the blocks and find a free space.
+    int index {0};
+    int countAvailableBlocks {0};
+    bool needsDefragmentation {false};
+    while (countAvailableBlocks<=howManyBlocks && index<TOTAL_BLOCKS) {
+
+        if (index<=TOTAL_BLOCKS_80_PERCENT) {
+            needsDefragmentation = true;
+        }
+    }
+
+    // If 80% or more of the file system is unavailable, run defragmentation function.
+    // After that, traverse through the blocks again since the blocks have changed.
+    if (needsDefragmentation) {
+        _runDefragmentation(fileSystem);
+
+        // If no space is available even after defragmentation, print an error message.
+        // "[ERROR] File not saved; file system full.
+
+    } 
+
+    // If the function reaches this point, it means there's no available block.
+    return -1;
+}
+
+void storeFile (FileSystemContiguous fileSystem, string fileName, int numBytes) {
+    // Find whether or not the filesystem has enough space for the file.
+    // This function returns -1 if no block is available. On the other hand,
+    // if there's an available block, it returns the first block's index.
+    const int howManyBlocks = std::ceil(BLOCK_SIZE/numBytes);
+    int availableBlock = _getAvailableBlock(fileSystem,howManyBlocks);
+    if (availableBlock==-1) {
+        cout<<"[ERROR] File not saved; file system full.\n";
+        return;
+    }
+
+    // Store the file.
+    const int availableBlockEnd = availableBlock+(howManyBlocks-1);
+    for (int i=availableBlock;i<=availableBlockEnd;++i) {
+        fileSystem.whichFileThisIsMappedTo[i]=fileName;
+    }
+
+    // Report the number of blocks used for storing the file.
+    cout<<"[RESULTS] ./\""<<fileName<<"\"\n";
+    cout<<"          Number of blocks used for storing this file: "<<howManyBlocks<<'\n';
+}
+
+void storeFile (FileSystemLinkedList fileSystem, string fileName, int numBytes) {
+
+}
+
+void storeFile (FileSystemLinkedListFAT fileSystem, string fileName, int numBytes) {
 
 }
 
 
+
 // TODO: Implement: {dir, store filename numBytes, access filename, del filename, dump, dump-all, help, exit}
-// TODO: Specifications: {print "File not saved; file system full" if store operation fails}
 
 // TODO: Notes: dir command lists all files and their attributes.
 // TODO: Notes: dump command shows what the OS needs to access files in the system: Write as disk-block map.
 // TODO: Notes: dump-all command shows the info given by the dump command as well as a map of each file block (which file it maps to or "free")
-// TODO: Notes: store command also needs to report the number of blocks used for storing the file.
 // TODO: Notes: access command counts the number of blocks (and therefore blocks) required for reading a given file.
 
 // --------------------------------------------------------------------
@@ -88,8 +148,7 @@ FileSystemContiguous::FileSystemContiguous() {
     for (int i=0;i<TOTAL_BLOCKS;++i) {
         // -1 here means not used. All numbers from 0 to any
         // positive number indicates the file it's mapped to.
-        whichFileThisIsMappedTo.push_back(-1);
-        whichFileName.push_back("-1");;
+        whichFileThisIsMappedTo.push_back("-1");
     }
 }
 
@@ -117,10 +176,21 @@ int main () {
     FileSystemLinkedList fileSystemLinkedList;
     FileSystemLinkedListFAT fileSystemLinkedListFAT;
 
+    // Switch to stdin redirection later
+    const string FILE_NAME_0 = "file_0";
+    const string FILE_NAME_1 = "file_1";
+    const string FILE_NAME_2 = "file_2";
+    const int FILE_SIZE_0 = 20;
+    const int FILE_SIZE_1 = 40;
+    const int FILE_SIZE_2 = 60;
+
+    storeFile(fileSystemContiguous,FILE_NAME_0,FILE_SIZE_0);
+    storeFile(fileSystemContiguous,FILE_NAME_1,FILE_SIZE_1);
+    storeFile(fileSystemContiguous,FILE_NAME_2,FILE_SIZE_2);
+
     // ----------------------------------------------------------------
     // Examples
     // ----------------------------------------------------------------
-
 
     return 0;
 }
