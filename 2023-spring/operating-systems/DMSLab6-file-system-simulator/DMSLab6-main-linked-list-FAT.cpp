b7c92a3e00eb86881a27ff8515bd0dc8d4dd2325
Deepak <dkrishnaagovindarajan21@ole.augie.edu>
2023-04-17 21:14:54 -0500

    feat: initialize fileAllocationTable and add startBlock

--- 2023-spring/operating-systems/DMSLab6-file-system-simulator/DMSLab6-main-linked-list-FAT.cpp
+++ 2023-spring/operating-systems/DMSLab6-file-system-simulator/DMSLab6-main-linked-list-FAT.cpp
@@ -9,6 +9,9 @@ Lab 6: File System Simulator
 
 #include "./include/DMSLab6-file-system-simulator.hpp"
 
+// Initialize the fileAllocationTable
+vector<int> FileSystemLinkedListFAT::fileAllocationTable = vector<int>(TOTAL_BLOCKS, -1);
+
 int main()
 {
     // ----------------------------------------------------------------
