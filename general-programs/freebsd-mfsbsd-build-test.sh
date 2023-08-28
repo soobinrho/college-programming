49e3426e78ebf680273e8632679efea5952d4205
Soobin Rho <soobinrho@gmail.com>
2023-08-27 23:19:25 -0500

    feat: repeat mfsBSD build 10 times for debugging
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- general-programs/freebsd-mfsbsd-build-test.sh
+++ general-programs/freebsd-mfsbsd-build-test.sh
@@ -5,21 +5,29 @@ alias DATE="date '+%Y%m%d_%H%M'"
 cd /usr/src
 mkdir -p /logs_mfsbsd
 time make buildworld -j12 \
-        > /logs_mfsbsd/buildworld_$(DATE).log 2>&1
+        > /logs_mfsbsd/buildWorldKernel_$(DATE).log 2>&1
 time make buildkernel -j6 \
-        > /logs_mfsbsd/buildkernel_$(DATE).log 2>&1
+        >> /logs_mfsbsd/buildWorldKernel$(DATE).log 2>&1
 
 cd ./release
-make list-mfsbsd
-time make release WITH_MFSBSD=1 \
-        > /logs_mfsbsd/release_$(DATE).log 2>&1
 
-mkdir -p ./images
-make install WITH_MFSBSD=1 DESTDIR=/usr/src/release/images \
-        > /logs_mfsbsd/install_$(DATE).log 2>&1
+COUNT=0
+while [ $COUNT -lt 10 ]
+do
+    time make release WITH_MFSBSD=1 \
+            > /logs_mfsbsd/release_$(DATE).log 2>&1
 
-ls -lh ./images/mfsbsdimages
-make clean && cd .. && make clean
-echo "--------------------------------------------------------------"
-echo "> Completed at $(DATE)"
-echo "--------------------------------------------------------------"
+    mkdir -p ./images
+    make install WITH_MFSBSD=1 DESTDIR=/usr/src/release/images \
+            > /logs_mfsbsd/install_$(DATE).log 2>&1
+
+    echo "--------------------------------------------------------------"
+    echo "> Completed at $(DATE)"
+    echo "--------------------------------------------------------------"
+
+    rm -rf ./images
+    rm /usr/obj/usr/src/amd64.amd64/release/mfsbsd*
+    make mfsbsd-clean
+
+    COUNT=$(expr $COUNT + 1)
+done
